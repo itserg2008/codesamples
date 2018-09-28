cb4e13bb31adc6b454010e3f1805e5a96890eced
itserg <itserg2008@gmail.com>
2018-09-28 09:37:51 +0300

    AutoMapper example

---
+++ dotnet/automapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AutoMapper;
+
+namespace am {
+
+    class Foo {
+        public int Data { get; set; }
+    }
+
+    class FooDto {
+        public int Data { get; set; }
+    }
+
+    class Program {
+        static void Main (string[] args) {
+
+            Mapper.Initialize (cfg => {
+                cfg.CreateMap<Foo, FooDto> ();
+            });
+
+            var foo = new Foo {
+                Data = 5
+            };
+
+            var fooDto = Mapper.Map<FooDto> (foo);
+
+            Console.WriteLine (fooDto.Data);
+        }
+    }
+}
---
+++ dotnet/automapper/readme.md
@@ -0,0 +1,5 @@
+Example of using AutoMapper
+
+```
+dotnet add package AutoMapper
+```
